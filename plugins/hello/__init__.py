b53eaf428245cae39ed133e8ce46ec81621106c4
051-lab <darius.vinson@tech901.org>
2026-04-29 00:34:47 -0500

    feat: add plugins/hello - bundled Hello World example plugin

---
+++ plugins/hello/__init__.py
@@ -0,0 +1,11 @@
+"""Hello World - bundled example plugin for WinForge."""
+
+PLUGIN_NAME = "Hello World"
+PLUGIN_VERSION = "1.0.0"
+PLUGIN_DESCRIPTION = "A simple hello-world plugin bundled with WinForge."
+PLUGIN_AUTHOR = "051-lab"
+
+
+def run():
+    """Plugin entry point."""
+    return "Hello from WinForge plugin system!"
